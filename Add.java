3ef4b5f28e56a6ba8fb03e9cc3fcdd92b2463aa3
Niroop C R Jain <160511355+niroop27@users.noreply.github.com>
2026-05-23 12:04:09 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -4,7 +4,7 @@ public class Add {
         int secondNumber = 50;
         int sum = firstNumber + secondNumber;
 
-        System.out.prin("CircleCI demo pipeline ran successfully.");
-        System.out.print(firstNumber + " + " + secondNumber + " = " + sum);
+        System.out.println("CircleCI demo pipeline ran successfully.");
+        System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
     }
 }
